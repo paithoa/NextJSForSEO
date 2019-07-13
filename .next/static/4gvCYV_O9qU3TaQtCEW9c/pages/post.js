bff430813fd4dfb51457dce0682683c4c40764b6
hhas0002 <hhas0002@student.monash.edu>
2019-07-14 03:24:43 +1000

    first

---
+++ .next/static/4gvCYV_O9qU3TaQtCEW9c/pages/post.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{EhLH:function(n,t,e){"use strict";e.r(t);var u=e("mXGw"),o=e.n(u);t.default=function(n){return o.a.createElement("h1",null,n.url.query.title)}},MDTK:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/post",function(){var n=e("EhLH");return{page:n.default||n}}])}},[["MDTK",1,0]]]);
---
+++ pages/post.js
@@ -0,0 +1,2 @@
+export default props => <h1>{props.url.query.title}</h1>
+
